ecd8697b819836f066cd810ba516b35e00a25059
Ira Herman <ira.herman@gmail.com>
2022-08-11 20:12:56 -0500

    Outputs board

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,72 @@
 extern crate cfonts;
-use cfonts::{render, Options, BgColors, Fonts, Align};
+use cfonts::{Align, BgColors, Fonts, Options};
 
-fn main() {
-	let thing: f64 = 0.5;
-	let thong: f32 = 0.5;
-	// println!("Hello, world! NOT READ ONLY ANYMOOOAARRR {:?}", thing>thong);
-	let header = draw();
-	println!("{}", header);
+#[derive(Debug, Clone, Copy)]
+enum CellState {
+    Empty,
+    Ex,
+    Oh,
 }
 
-fn draw() -> String {
-	// Return string
-	let output = render(Options {
-		text: String::from("Tic Tac Toe"),
-		gradient: vec![String::from("#ff0000"),String::from("#ff0000")],
-		background: BgColors::Black,
-		font: Fonts::FontBlock,
-		align: Align::Center,
-		..Options::default()
-	});
+type State = [[CellState; 3]; 3];
 
-	output.text
+fn main() {
+    let thing: f64 = 0.5;
+    let thong: f32 = 0.5;
+    // println!("Hello, world! NOT READ ONLY ANYMOOOAARRR {:?}", thing>thong);
+
+    let app_state: State = [
+		[CellState::Empty,CellState::Empty,CellState::Empty],
+		[CellState::Ex,CellState::Ex,CellState::Empty],
+		[CellState::Empty,CellState::Oh,CellState::Empty],
+	];
+
+    let header = draw(&app_state);
+    println!("{}", header);
+}
+
+fn draw(state: &State) -> String {
+    // Return string
+    let logo = cfonts::render(Options {
+        text: String::from("Tic Tac Toe"),
+        gradient: vec![String::from("#ff0000"), String::from("#ff0000")],
+        background: BgColors::Black,
+        font: Fonts::FontBlock,
+        align: Align::Center,
+        ..Options::default()
+    });
+
+	let mut output = logo.text.clone();
+	output += &format!("\n\n ╔═══╦═══╦═══╗\n ║");
+
+	for (i, line) in state.iter().enumerate() {
+		for cell in line.iter() {
+			let this_cell = match cell {
+				CellState::Empty =>  "  ".to_string(),
+				CellState::Ex =>  " X".to_string(),
+				CellState::Oh => " O".to_string(),
+			};
+			output += &format!("{} ║", this_cell);
+		}
+		if i != 2 {
+			output += &format!("\n ╠═══╬═══╬═══╣\n ║");
+		} else {
+			output += &format!("\n");
+		}
+	}
+	output += &format!(" ╚═══╩═══╩═══╝");
+
+    // "
+    // ╔═══╦═══╦═══╗
+    // ║ X ║   ║   ║
+    // ╠═══╬═══╬═══╣
+    // ║ X ║ O ║ X ║
+    // ╠═══╬═══╬═══╣
+    // ║ X ║ O ║   ║
+    // ╚═══╩═══╩═══╝
+    // "
+
+    output
 }
 
 // 'c' // <-- char
