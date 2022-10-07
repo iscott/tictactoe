86c69dbf8fcf221aa4f36a6e6ab6949adccd87a6
Ira Herman <ira.herman@gmail.com>
2022-10-06 20:10:48 -0500

    Adds termion for using arrow keys and redrawing state

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,42 @@
+//! Hello world
 extern crate cfonts;
+extern crate termion;
+
+use std::io::{stdin, stdout, Write};
+use termion::event::Key;
+use termion::input::TermRead;
+use termion::raw::IntoRawMode;
+
 use cfonts::{Align, BgColors, Fonts, Options};
+
+#[derive(Debug, Clone, Copy)]
+enum PrevState {
+    Empty,
+    Ex,
+    Oh,
+}
 
 #[derive(Debug, Clone, Copy)]
 enum CellState {
     Empty,
     Ex,
     Oh,
+    Me(PrevState),
 }
 
 type State = [[CellState; 3]; 3];
 
 fn main() {
-    let thing: f64 = 0.5;
-    let thong: f32 = 0.5;
+    // let thing: f64 = 0.5;
+    // let thong: f32 = 0.5;
     // println!("Hello, world! NOT READ ONLY ANYMOOOAARRR {:?}", thing>thong);
 
     let app_state: State = [
-		[CellState::Empty,CellState::Empty,CellState::Empty],
+		[CellState::Me(PrevState::Empty),CellState::Empty,CellState::Empty],
 		[CellState::Ex,CellState::Ex,CellState::Empty],
 		[CellState::Empty,CellState::Oh,CellState::Empty],
 	];
 
-    let header = draw(&app_state);
-    println!("{}", header);
-}
-
-fn draw(state: &State) -> String {
     // Return string
     let logo = cfonts::render(Options {
         text: String::from("Tic Tac Toe"),
@@ -36,7 +47,81 @@ fn draw(state: &State) -> String {
         ..Options::default()
     });
 
-	let mut output = logo.text.clone();
+    let header = draw(&app_state);
+
+    // let size = termion::terminal_size();
+
+	// if let Ok((width, height)) = size {
+	// 	if width < min_width || height < min_height {
+	// 		panic!("\r\n\r\n{}This terminal is not big enough width width:{} height:{}\r\nTo play Battlefield you need at least width:{} height:{}{}\r\n\r\n", termion::color::Fg(termion::color::Red), width, height, min_width, min_height, termion::color::Fg(termion::color::Reset));
+	// 	}
+	// } else {
+	// 	panic!("The size of the terminal can't be determined");
+	// }
+
+    let mut stdout = stdout().into_raw_mode().unwrap();
+
+    write!(stdout, "{}{}", termion::color::Bg(termion::color::Black), termion::clear::All).unwrap();
+	stdout.flush().unwrap();
+    
+    write!(
+		stdout,
+		"{}{}{}{}{}{}{}",
+		termion::clear::AfterCursor,
+		termion::cursor::Goto(1, 2),
+		termion::color::Fg(termion::color::White),
+		termion::cursor::Hide,
+		logo.text,
+		header,
+		termion::cursor::Save
+	)
+	.unwrap();
+	stdout.flush().unwrap();
+
+
+    for key in stdin().keys() {
+		match key.unwrap() {
+			Key::Esc | Key::Char('q') => {
+				write!(stdout, "{}{}", termion::cursor::Restore, termion::cursor::Show).unwrap();
+				stdout.flush().unwrap();
+				termion::raw::RawTerminal::suspend_raw_mode(&stdout).unwrap();
+				std::process::exit(0);
+			}
+			Key::Char('r') => {}
+            // MOVEMENT
+			Key::Left => {
+				// do work
+			}
+			Key::Right => {
+				// do work
+			}
+			Key::Up => {
+				// do work
+			}
+			Key::Down => {
+				// do work
+            }
+            _ => {}
+        }
+        // draw everything
+    }
+
+}
+
+/// This draw function draws our state to a human display thongo
+/// ```rust
+/// let state = [[CellState::Empty; 3]; 3];
+/// let result = "\n\n ╔═══╦═══╦═══╗
+/// ║   ║   ║   ║
+/// ╠═══╬═══╬═══╣
+/// ║   ║   ║   ║
+/// ╠═══╬═══╬═══╣
+/// ║   ║   ║   ║
+/// ╚═══╩═══╩═══╝";
+/// assert_eq!(draw(&state), result);
+/// ```
+fn draw(state: &State) -> String {
+	let mut output = String::new();
 	output += &format!("\n\n ╔═══╦═══╦═══╗\n ║");
 
 	for (i, line) in state.iter().enumerate() {
@@ -45,6 +130,7 @@ fn draw(state: &State) -> String {
 				CellState::Empty =>  "  ".to_string(),
 				CellState::Ex =>  " X".to_string(),
 				CellState::Oh => " O".to_string(),
+                CellState::Me(_) => " •".to_string(),
 			};
 			output += &format!("{} ║", this_cell);
 		}
@@ -69,6 +155,30 @@ fn draw(state: &State) -> String {
     output
 }
 
+#[test]
+fn draw_test() {
+    let mut state = [[CellState::Empty; 3]; 3];
+    let mut result = "\n\n ╔═══╦═══╦═══╗
+ ║   ║   ║   ║
+ ╠═══╬═══╬═══╣
+ ║   ║   ║   ║
+ ╠═══╬═══╬═══╣
+ ║   ║   ║   ║
+ ╚═══╩═══╩═══╝";
+    assert_eq!(draw(&state), result);
+
+    state = [[CellState::Empty; 3]; 3];
+    state[1][1] = CellState::Ex;
+    state[1][2] = CellState::Oh;
+    result = "\n\n ╔═══╦═══╦═══╗
+ ║   ║   ║   ║
+ ╠═══╬═══╬═══╣
+ ║   ║ X ║ O ║
+ ╠═══╬═══╬═══╣
+ ║   ║   ║   ║
+ ╚═══╩═══╩═══╝";
+    assert_eq!(draw(&state), result);
+}
 // 'c' // <-- char
 // "str" // <-- str
 // let my_string = "string".to_string() // <-- String
